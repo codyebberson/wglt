4b9b9690df8992fecfc61fdbb887b0029078d5e4
Cody Ebberson <cody@ebberson.com>
2019-03-03 15:27:35 -0800

    Version 0.1.27

--- dist/gui/itemcontainerdialog.d.ts
+++ dist/gui/itemcontainerdialog.d.ts
@@ -1,11 +1,13 @@
 import { Item } from '../item';
+import { Message } from '../message';
 import { Rect } from '../rect';
 import { XArray } from '../xarray';
 import { Dialog } from './dialog';
 export declare class ItemContainerDialog extends Dialog {
+    readonly messages: Message[];
     readonly capacity: number;
     readonly items: XArray<Item>;
-    constructor(rect: Rect, capacity: number, items: XArray<Item>);
+    constructor(rect: Rect, messages: Message[], capacity: number, items: XArray<Item>);
     private addItem;
     private removeItem;
     private getExistingButton;
--- dist/gui/talentsdialog.d.ts
+++ dist/gui/talentsdialog.d.ts
@@ -1,11 +1,13 @@
+import { Message } from '../message';
 import { Rect } from '../rect';
 import { Talent } from '../talent';
 import { XArray } from '../xarray';
 import { Dialog } from './dialog';
 export declare class TalentsDialog extends Dialog {
+    readonly messages: Message[];
     readonly capacity: number;
     readonly talents: XArray<Talent>;
-    constructor(rect: Rect, capacity: number, talents: XArray<Talent>);
+    constructor(rect: Rect, messages: Message[], capacity: number, talents: XArray<Talent>);
     private addItem;
     private removeItem;
     private getNextFreeSlot;
