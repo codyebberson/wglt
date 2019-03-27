ac6422d377d3c437244afc6d841976f9255ef1e9
Cody Ebberson <cody@ebberson.com>
2019-03-27 12:08:44 -0700

    Version 0.1.52

--- dist/item.d.ts
+++ dist/item.d.ts
@@ -3,6 +3,16 @@ import { Entity } from './entity';
 import { Message } from './message';
 export declare class Item extends Entity {
     tooltipMessages?: Message[];
+    /**
+     * Returns true if this item can be stacked with the other item
+     * in containers such as chests and bags.
+     *
+     * By default, items are stackable if they have the same name.
+     * Overriding classes can change this logic.
+     *
+     * @param other Other item to stack with.
+     */
+    isStackable(other: Item): boolean;
     onBump(player: Actor): boolean;
     onPickup(user: Actor): void;
     onUse(user: Actor): boolean;
