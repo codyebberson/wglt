9b607b7226afcd72b751d092525a10f2497a3335
Cody Ebberson <cody@ebberson.com>
2019-04-21 17:24:50 -0700

    Version 0.1.59

--- dist/serializemetadata.d.ts
+++ dist/serializemetadata.d.ts
@@ -1,8 +1,14 @@
 export declare function createSerializeMetadata(ctor: any): SerializeMetadata;
 export declare function getSerializeMetadata(ctor: any): SerializeMetadata;
 export declare class SerializeMetadata {
+    ctor: {
+        new (): any;
+    };
     className: string;
     valueType: boolean;
     customSerializer?: Function;
     customDeserializer?: Function;
+    constructor(ctor: {
+        new (): any;
+    });
 }
---
+++ dist/serializer.d.ts
@@ -0,0 +1,6 @@
+export declare class Serializer {
+    readonly typeLists: any;
+    serialize(obj: any): any;
+    private serializeObject;
+    private serializeArray;
+}
