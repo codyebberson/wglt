85a7561624bc6818a280df654861bf5527924cf6
Cody Ebberson <cody@ebberson.com>
2019-04-24 18:29:58 -0700

    Version 0.1.60

--- dist/gui/itemcontainerbuttonslot.d.ts
+++ dist/gui/itemcontainerbuttonslot.d.ts
@@ -1,11 +1,8 @@
-import { ArrayList } from '../arraylist';
-import { Item } from '../item';
 import { Key } from '../keys';
 import { Rect } from '../rect';
 import { ButtonSlot } from './buttonslot';
 import { Panel } from './panel';
 export declare class ItemContainerButtonSlot extends ButtonSlot {
-    readonly items: ArrayList<Item>;
-    constructor(rect: Rect, items: ArrayList<Item>, shortcutKey?: Key);
+    constructor(rect: Rect, shortcutKey?: Key);
     onDrop(panel: Panel): boolean;
 }
