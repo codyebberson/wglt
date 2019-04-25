2d94904542dadb9a9c414d239582daee6d8d14fb
Cody Ebberson <cody@ebberson.com>
2019-04-25 10:52:40 -0700

    Version 0.1.61

--- dist/compoundmessage.d.ts
+++ dist/compoundmessage.d.ts
@@ -1,8 +1,10 @@
 import { App } from './app';
 import { Vec2 } from './vec2';
 import { Message } from './message';
+import { Font } from './font';
 export declare class CompoundMessage extends Message {
     readonly messages: Message[];
     constructor(...messages: Message[]);
     draw(app: App, pos: Vec2): void;
+    getWidth(font: Font): number;
 }
--- dist/message.d.ts
+++ dist/message.d.ts
@@ -1,9 +1,11 @@
 import { Color } from './color';
 import { App } from './app';
 import { Vec2 } from './vec2';
+import { Font } from './font';
 export declare class Message {
     readonly text: string;
     readonly color: Color;
     constructor(text: string, color: Color);
     draw(app: App, pos: Vec2): void;
+    getWidth(font: Font): number;
 }
