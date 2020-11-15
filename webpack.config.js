b0f07c9ab0a29b9b4dcd15d34b96be8bfb2954de
Cody Ebberson <cody@ebberson.com>
2020-11-15 15:29:34 -0800

    More aggressive minimizer

--- webpack.config.js
+++ webpack.config.js
@@ -32,6 +32,28 @@ module.exports = entries.map(entry => ({
     libraryTarget: 'umd'
   },
   optimization: {
-    minimizer: [new Terser()]
+    minimizer: [new Terser({
+      terserOptions: {
+        parse: {
+          ecma: 8,
+        },
+        compress: {
+          ecma: 6,
+          warnings: false,
+          comparisons: false,
+          inline: 2
+        },
+        mangle: {
+          properties: {
+            keep_quoted: true
+          }
+        },
+        output: {
+          ecma: 6,
+          comments: false,
+          ascii_only: true
+        }
+      }
+    })]
   }
 }));
