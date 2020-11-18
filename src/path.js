9acfaf84b2352cf41c3037db9c10be608cd82fc1
Cody Ebberson <cody@ebberson.com>
2020-11-17 17:14:53 -0800

    Fixed bug in path finding

--- src/path.js
+++ src/path.js
@@ -1,7 +1,7 @@
 
 const dxs = [-1, 0, 1, -1, 1, -1, 0, 1];
 const dys = [-1, -1, -1, 0, 0, 1, 1, 1];
-const costs = [1.5, 1, 1.5, 1, 1, 1.5, 1, 1.5];
+const costs = [1.4, 1, 1.4, 1, 1, 1.4, 1, 1.4];
 let pathId = 0;
 
 /**
@@ -18,7 +18,7 @@ export function computePath(map, source, dest, maxDist) {
   const sourceCell = map.grid[source.y][source.x];
   sourceCell.pathId = pathId;
   sourceCell.g = 0.0;
-  sourceCell.h = Math.min(Math.abs(source.x - dest.x), Math.abs(source.y - dest.y));
+  sourceCell.h = Math.hypot(source.x - dest.x, source.y - dest.y);
   sourceCell.prev = null;
 
   const q = new SortedSet([sourceCell]);
@@ -35,14 +35,17 @@ export function computePath(map, source, dest, maxDist) {
       const y = u.y + dys[i];
       if (x >= 0 && x < map.width && y >= 0 && y < map.height) {
         const v = map.grid[y][x];
+        if (v.blocked && v.explored && (x !== dest.x || y !== dest.y)) {
+          continue;
+        }
         if (v.pathId !== pathId) {
           v.pathId = pathId;
           v.g = Infinity;
-          v.h = Math.min(Math.abs(x - dest.x), Math.abs(y - dest.y));
+          v.h = Math.hypot(x - dest.x, y - dest.y);
           v.prev = null;
         }
         const alt = u.g + costs[i];
-        if (alt < v.g && alt <= maxDist && (!map.grid[y][x].blocked || (x === dest.x && y === dest.y))) {
+        if (alt < v.g && alt <= maxDist) {
           v.g = alt;
           v.prev = u;
           q.insert(v);
@@ -73,17 +76,20 @@ class SortedSet {
     const array = this.values;
     let low = 0;
     let high = array.length;
+    let index = 0;
 
     while (low < high) {
       const mid = (low + high) >>> 1;
       const midCell = array[mid];
       if (midCell.g + midCell.h > cell.g + cell.h) {
         low = mid + 1;
+        index = low;
       } else {
         high = mid;
+        index = high;
       }
     }
-    array.splice(low + 1, 0, cell);
+    array.splice(index, 0, cell);
   }
 
   pop() {
