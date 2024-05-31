0e370ef294df3005294916a9c527d822d675effb
Cody Ebberson <cody@ebberson.com>
2024-05-31 14:21:50 -0700

    Cleanup

--- docs/examples/graphics/roguelike/quests/quest.ts
+++
@@ -1,9 +0,0 @@
-export class Quest {
-  description: string[];
-  xpGain: number;
-
-  constructor(description: string[], xpGain: number) {
-    this.description = description;
-    this.xpGain = xpGain;
-  }
-}
